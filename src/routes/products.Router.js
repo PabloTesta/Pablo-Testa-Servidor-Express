4bb83ff2b31e234b9df99afd7609315942900aab
Testa <pablogtesta94@gmail.com>
2023-02-21 20:40:55 -0300

    commit de products.Router

--- src/routes/products.Router.js
+++ src/routes/products.Router.js
@@ -5,43 +5,55 @@ const router = Router();
 const productManager= new ProductManager();
 
 
-let persisitirUsuario= async (response)=>{
-    try{
-        await productManager.addProduct("Producto prueba","Esto es un producto prueba",200,"sin imagen","abc123",25); 
-        console.log('Creando Usuario');
-        let prod= await productManager.getProducts();
-        console.log('llamando a consultar usuarios');
-        console.log('haciendo STRINGIFY');
-        console.log(prod);
-        return prod
-    }catch{
-        response.status(400).send('Error guardando usuario');
-    }
-  }
+router.post('/product',async(request,response )=>{
+    let user=request.body;//lo que llegue, llegará como un JSON en el body 
+ await productManager.addProduct("Producto prueba cuatro","Esto es un producto prueba",200,"sin imagen","abc123",32); 
 
-  router.get ('/', async (request,response)   => {
-    console.log('Llamando a crear usuario');
-    let result = await persisitirUsuario(response);
-    console.log('REsolviendo Promesa');
-    console.log(result);
-    response.send(result)
-    /* let {limit} = request.query; 
-     const prod = productManager.getProducts() || []; 
-         if(limit){
-            function limitar(prod,limite){
-                return prod.slice(0,limite)
-            }
-            response.send(limitar(prod,1))
-        }  */
-     
+console.log(users);
+response.status(200).send({status:'Success',message:`Todo salío bien, el cliente envió la petición, es decir, hizo un request, envió el user/usuario en el correspondiente body, llegó en un JSON porque nosotros se lo forzamos desde postman y lo pusheamos a nuestro arreglo `})
+})
+
+router.get('/api/products',async(request,response)=>{
+    console.log('Consumiendo api GET /api/users...');
+    console.log('Usuarios actuales: ');
+    users= await productManager.getProducts();
+    console.log(JSON.parse(users));
+    console.log('ahora mandamos el arreglo de usuarios a traves de un RESPONSE al cliente que lo solicita a traves del método GET. Notar que SE LO MANDAMOS con un RESPONSE, esta vez no es un REQUEST, el único que usa request es el cliente para con el servidor y manda lo que tiene que mandar en un BODY. ');
+
+    response.status(200).send(users);
     });
-
-     router.get('/:prodId',(request,response)=>{
-        const prod = productManager.getProductById(request.params.userId);
-        if(prod){
-            response.send(JSON.stringify(prod));
+    router.get('/api/products/:productId',async(request,response)=>{
+        
+        console.log(request.params);
+        let userId=parseInt(request.params.userId)
+        const products= await productManager.getProducts();
+        console.log(JSON.parse(products));
+        const productId= products.find(u=>u.id===userId)
+        
+        console.log('mostrando el usuario encontrado: ');
+        console.log(JSON.stringify(users[productId]))
+        if(productId){
+            response.status(200).send(users[productId])
+        }else{
+        response.status(400).send({error:400,message:"No existe un producto con ese id"})
         }
-        response.send({message:"El producto con ese ID no se encuentra"})
+    });
+    router.delete('/api/product/:userId', (request,response)=>{
+        console.log('-------------Consumiendo api DELETE /api/user..---------------');
+        console.log(request.params);
+        let userId=parseInt(request.params.userId);
+        console.log(`Buscando Usuario a eliminar por ${request.params}`);
+        const usersSize=users.length;
+        const userPosition= users.findIndex((u=>u.id===userId))
+        if(userPosition<0){
+            return response.status(202).send({status:'info', error:'Usuario no encontrado'});
+        }
+        console.log('Usuario encontrado para eliminar');
+        console.log(users[userPosition]);
+        users.splice(userPosition,1);
+        if(users.length===usersSize){
+            return response.status(500).send({status:"error",message:"El servidor no pudo borrar el usuario"})
+        }
+        return response.send({status:"Success",message:"Usuario Eliminado"});
     })
- 
 export default router;
